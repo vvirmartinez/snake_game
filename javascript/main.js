e435967284440a4fc172e10dc34758937e061999
PolRibas <polribasrovira@gmail.com>
2024-02-13 20:41:54 +0100

    change title

---
+++ javascript/main.js
@@ -0,0 +1,16 @@
+const main = () => {
+    const root = document.getElementById('root');
+
+    const createSpawnPage = () => {
+        const spawnPage = document.createElement('div');
+        spawnPage.id = 'spawn-page';
+        spawnPage.innerHTML = `
+            <h1>Spawn Page</h1>
+            <button id="spawn-button">Spawn</button>
+        `;
+        root.appendChild(spawnPage);
+    }
+    createSpawnPage()
+}
+
+document.addEventListener('DOMContentLoaded', main);
