b030d28dce085cf283b31401208196fc5c546cee
PolRibas <polribasrovira@gmail.com>
2024-02-20 21:34:16 +0100

    dummy commit

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,26 @@
 const main = () => {
     const root = document.getElementById('root');
 
+    const gameOverPage = () => {
+        root.innerHTML = `
+            <h1>Game Over</h1>
+            <button id="restart-button" class="vir-class">Restart</button>
+        `;
+        document.getElementById('restart-button').addEventListener('click', createGamePage);
+    }
+
+    const createGamePage = () => {
+        root.innerHTML = `
+            <canvas id="game-canvas" width="${CANVAS_SQUERE}" height="${CANVAS_SQUERE}">
+        `;
+
+        const canvas = document.getElementById('game-canvas');
+        
+        const game = new Game(canvas, gameOverPage);
+
+        game.start();
+    }
+
     const createSpawnPage = () => {
         const spawnPage = document.createElement('div');
         spawnPage.id = 'spawn-page';
@@ -9,7 +29,11 @@ const main = () => {
             <button id="spawn-button">Spawn</button>
         `;
         root.appendChild(spawnPage);
+        document.getElementById('spawn-button').addEventListener('click', createGamePage);
     }
+
+
+
 
     
     createSpawnPage()
